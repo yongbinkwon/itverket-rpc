90b105e2e54c857f58553757c52701966281471e
Yong Bin Kwon <yongbinkwon97@gmail.com>
2023-08-08 18:58:29 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    id("org.springframework.boot") version "3.1.2"
+    application
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+group = "no.itverket"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClass.set("no.itverket.rpc.ApplicationKt")
+}
