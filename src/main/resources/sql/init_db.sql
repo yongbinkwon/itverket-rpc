37fbd94c8c4ac0bf300de43224e67570e1572f8f
yongbinkwon <yongbinkwon97@gmail.com>
2023-08-21 14:58:53 +0200

    add postgres db + table for scheduled matches

---
+++ src/main/resources/sql/init_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ScheduledMatch
+(
+    Id   int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    Sign varchar(255) NOT NULL,
+    Team varchar(255) NOT NULL,
+    Processed boolean NOT NULL DEFAULT FALSE
+);
